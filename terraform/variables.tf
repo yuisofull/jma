df4698ad04e903a7c030376e925d96fcaa30f9df
Nana Janashia <nn_janashia@yahoo.com>
2020-12-19 10:56:16 +0100

    add terraform config for deploying to ec2

---
+++ terraform/variables.tf
@@ -0,0 +1,21 @@
+variable vpc_cidr_block {
+    default = "10.0.0.0/16"
+}
+variable subnet_cidr_block {
+    default = "10.0.10.0/24"
+}
+variable avail_zone {
+    default = "eu-west-3a"
+}
+variable env_prefix {
+    default = "dev"
+}
+variable my_ip {
+    default = "212.124.154.110/32"
+}
+variable instance_type {
+    default = "t2.micro"
+}
+variable region {
+    default = "eu-west-3"
+}
